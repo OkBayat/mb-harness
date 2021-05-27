24a83faa62c7226ddedc51f791e412ce8796fab2
Mohammad Bayat <OkBayat@gmail.com>
2021-05-27 16:37:08 +0430

    Remove init method

--- src/harness.ts
+++ src/harness.ts
@@ -1,35 +1,38 @@
-import {ComponentFixture, TestBed, TestBedStatic} from '@angular/core/testing';
+import {ComponentFixture, TestBedStatic} from '@angular/core/testing';
 import {select} from './select/select';
 import {selectAll} from './select-all/select-all';
 import {dispatchKeyboardEvent} from './dispatch-events/dispatch-keyboard-event';
-
-function init<T>(testBed: TestBedStatic): Harness<T> {
-    this.testBed = testBed;
-    this.fixture = testBed.createComponent(this.__component);
-    this.component = this.fixture.componentInstance;
-    this.fixture.detectChanges();
-    this.select = select.bind(this);
-    this.selectAll = selectAll.bind(this);
-    this.dispatchKeyboardEvent = dispatchKeyboardEvent.bind(this);
-
-    return this;
-}
 
 export function MbHarness(component) {
     return (target) => {
         const self = target.prototype;
         self.__component = component;
         self.__elm = null;
-        self.init = init.bind(self);
     }
 }
 
 export class Harness<T> {
-    testBed: TestBed;
     fixture: ComponentFixture<T>;
     component: T;
-    init = init;
     select = select;
     selectAll = selectAll;
     dispatchKeyboardEvent = dispatchKeyboardEvent;
+
+    /* tslint:disable:variable-name */
+    private __component;
+    private __elm;
+
+    constructor(public testBed: TestBedStatic) {
+        this.init();
+    }
+
+    private init(): void {
+        this.select = select.bind(this);
+        this.selectAll = selectAll.bind(this);
+        this.dispatchKeyboardEvent = dispatchKeyboardEvent.bind(this);
+
+        this.fixture = this.testBed.createComponent(this.__component);
+        this.component = this.fixture.componentInstance;
+        this.fixture.detectChanges();
+    }
 }
